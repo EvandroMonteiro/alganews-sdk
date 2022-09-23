013160335faee672160065e7ed2cc522a639781d
Evandro Lima <evandro.lima@gat.digital>
2022-09-23 20:44:53 +0000

    Set payment sort opcional

--- src/@types/Payment.ts
+++ src/@types/Payment.ts
@@ -1,13 +1,15 @@
 import { AlgaNews } from "./AlgaNews";
 
 export namespace Payment {
-  export type Input = AlgaNews.components['schemas']['PaymentInput']
-  export type Detailed = AlgaNews.components['schemas']['PaymentDetailed']
-  export type Summary = AlgaNews.components['schemas']['PaymentSummary']
-  export type Preview = AlgaNews.components['schemas']['PaymentPreview']
-  export type PreviewInput = AlgaNews.components['schemas']['PaymentPreviewInput']
-  export type Paginated = AlgaNews.components['schemas']['PaymentsPaginated']
-  export type PostWithEarnings = AlgaNews.components['schemas']['PostWithEarnings']
+  export type Input = AlgaNews.components["schemas"]["PaymentInput"];
+  export type Detailed = AlgaNews.components["schemas"]["PaymentDetailed"];
+  export type Summary = AlgaNews.components["schemas"]["PaymentSummary"];
+  export type Preview = AlgaNews.components["schemas"]["PaymentPreview"];
+  export type PreviewInput =
+    AlgaNews.components["schemas"]["PaymentPreviewInput"];
+  export type Paginated = AlgaNews.components["schemas"]["PaymentsPaginated"];
+  export type PostWithEarnings =
+    AlgaNews.components["schemas"]["PostWithEarnings"];
 
   export type Query = {
     payeeId?: number;
@@ -15,6 +17,6 @@ export namespace Payment {
     scheduledToYearMonth?: string;
     page?: number;
     size?: number;
-    sort: [keyof Summary, 'asc' | 'desc'];
+    sort?: [keyof Summary, "asc" | "desc"];
   };
 }
